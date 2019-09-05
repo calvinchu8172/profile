2c2dbfacac95737eae19fbafbd7a86aab102b112
Calvin Chu <calvinchu@ecoworkinc.com>
2019-09-05 18:49:08 +0800

    Add server up command in start.sh.

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 eval whenever -w 
-# eval bundle exec unicorn -p 3000 -c config/unicorn/production.rb
+eval bundle exec unicorn -p 3000 -c config/unicorn/production.rb
 # eval rake device:register
 # eval bundle exec unicorn -p 3000 -c config/unicorn/production.rb &
 # eval whenever -w
