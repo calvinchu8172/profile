cec32221760f1af279be1511aeb058da5e160cf1
Calvin Chu <calvinchu@ecoworkinc.com>
2019-09-05 18:21:06 +0800

    Add homepage and Dockerfile.

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+eval whenever -w 
+# eval bundle exec unicorn -p 3000 -c config/unicorn/production.rb
+# eval rake device:register
+# eval bundle exec unicorn -p 3000 -c config/unicorn/production.rb &
+# eval whenever -w
